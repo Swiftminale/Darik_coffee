a36877bf9c1f70cafa55154c6ec81d4893438280
Swiftminale <minalefetene@gmail.com>
2024-07-10 14:11:05 +0300

    removed footer items

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,15 +7,7 @@ function Footer() {
   return (
     <div className="footer-container">
       <div class="footer-links">
-        <div className="footer-link-wrapper">
-          <div class="footer-link-items">
-            <h2>About Us</h2>
-            <Link to="/sign-up">How it works</Link>
-            <Link to="/">Testimonials</Link>
-            <Link to="/">Careers</Link>
-            <Link to="/">Investors</Link>
-            <Link to="/">Terms of Service</Link>
-          </div>
+        {/* <div className="footer-link-wrapper">
           <div class="footer-link-items">
             <h2>Contact Us</h2>
             <Link to="/">Contact</Link>
@@ -23,15 +15,7 @@ function Footer() {
             <Link to="/">Destinations</Link>
             <Link to="/">Sponsorships</Link>
           </div>
-        </div>
-        <div className="footer-link-wrapper">
-          <div class="footer-link-items">
-            <h2>Social Media</h2>
-            <Link to="/">Instagram</Link>
-            <Link to="/">Facebook</Link>
-            <Link to="/">Tiktok</Link>
-          </div>
-        </div>
+        </div> */}
       </div>
       <section class="social-media">
         <div class="social-media-wrap">

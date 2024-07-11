993baac915938c3a47d3bf0d3cfabe8c4e7af8b3
Swiftminale <minalefetene@gmail.com>
2024-07-11 16:17:59 +0300

    finishing Css 16:18

--- src/components/Requestform.js
+++ src/components/Requestform.js
@@ -1,6 +1,7 @@
 import React, { useRef } from "react";
 import emailjs from "@emailjs/browser";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link } from "react-router-dom";
 import "./RequestForm.css";
 import toast from "react-hot-toast";
 function Requestform() {
@@ -36,19 +37,28 @@ function Requestform() {
 
   return (
     <>
+      <div className="req_right">
+        <h5>Quality Beyond Your Expectation</h5>
+        <p>
+          Darik coffee for the past decade, Testi Specialty coffee established a
+          purpose, that’s been and will forever be providing our partners which
+          we consider part of the Testi family with the finest quality coffee
+          the world has to offer.
+        </p>
+        <p>______________________________________________</p>
+      </div>
       <div className="form_container">
         <form className="cf" ref={form} onSubmit={sendEmail}>
+          <h1>Request Sample</h1>
           <div className="half left cf">
-            <input type="text" placeholder="Name" name="user_name" />
-            <input type="email" placeholder="Email address" name="user_email" />
+            <input type="text" placeholder="Full Name" name="user_name" />
+            <input type="text" placeholder="Phone" name="user_name" />
           </div>
           <div className="half right cf">
-            <textarea
-              name="message"
-              type="text"
-              placeholder="Message"
-            ></textarea>
+            <input type="text" placeholder="Company Name" name="user_name" />
+            <input type="email" placeholder="Email address" name="user_email" />
           </div>
+          <textarea name="message" type="text" placeholder="Message"></textarea>
           <input type="submit" value="Request" id="input-submit" />
         </form>
       </div>

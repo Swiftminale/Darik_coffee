67361eb81c8d8c8febb2a847a632668a00455916
Swiftminale <minalefetene@gmail.com>
2024-07-18 23:56:08 +0300

    made changes to the footer and navbar

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,12 +3,14 @@ import "./Footer.css";
 import { faCoffee } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Link } from "react-router-dom";
+import Logo_or from "../pic/image_png.png";
+
 function Footer() {
   return (
     <div className="footer-container">
-      <div class="footer-links">
+      <div className="footer-links">
         {/* <div className="footer-link-wrapper">
-          <div class="footer-link-items">
+          <div className="footer-link-items">
             <h2>Contact Us</h2>
             <Link to="/">Contact</Link>
             <Link to="/">Support</Link>
@@ -17,39 +19,42 @@ function Footer() {
           </div>
         </div> */}
       </div>
-      <section class="social-media">
-        <div class="social-media-wrap">
-          <div class="footer-logo">
+      <section className="social-media">
+        <div className="social-media-wrap">
+          <div>
+            <img src={Logo_or} alt="logo" className="footer-logo-img" />
+          </div>
+          <div className="footer-logo">
             <Link to="/" className="social-logo">
               DARIK
               <FontAwesomeIcon icon={faCoffee} />
             </Link>
           </div>
-          <small class="website-rights">Darik © 2020</small>
-          <div class="social-icons">
+          <small className="website-rights">Darik © 2020</small>
+          <div className="social-icons">
             <Link
-              class="social-icon-link facebook"
+              className="social-icon-link facebook"
               to="/"
               target="_blank"
               aria-label="Facebook"
             >
-              <i class="fab fa-facebook-f" />
+              <i className="fab fa-facebook-f" />
             </Link>
             <Link
-              class="social-icon-link instagram"
+              className="social-icon-link instagram"
               to="/"
               target="_blank"
               aria-label="Instagram"
             >
-              <i class="fab fa-instagram" />
+              <i className="fab fa-instagram" />
             </Link>
             <Link
-              class="social-icon-link LinkedIn"
+              className="social-icon-link linkedin"
               to="/"
               target="_blank"
               aria-label="LinkedIn"
             >
-              <i class="fab fa-linkedin" />
+              <i className="fab fa-linkedin" />
             </Link>
           </div>
         </div>
--- src/components/pages/Gallery.js
+++ src/components/pages/Gallery.js
@@ -1,13 +1,12 @@
 import React from "react";
 import "../../App.css";
 import Cards from "../Cards";
-
+import Footer from "../Footer";
 export default function Gallery() {
   return (
     <>
-      <div className="Gallery">
-        <Cards />
-      </div>
+      <div className="Gallery">{/* <Cards /> */}</div>
+      <Footer />
     </>
   );
 }

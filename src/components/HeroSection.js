672d4c4a7bc9f4eeefa41204b9ebe36058dc26cb
Swiftminale <minalefetene@gmail.com>
2024-07-19 01:12:26 +0300

    changed video speed in HeroSection

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import "../App.css";
 import { Button } from "./Button";
 import { Link } from "react-router-dom";
 import "./HeroSection.css";
 
 function HeroSection() {
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    if (videoRef.current) {
+      videoRef.current.playbackRate = 0.85; // Adjust the speed here
+    }
+  }, []);
+
   return (
     <div className="hero-container">
-      <video src="/videos/video-03.mp4" autoPlay loop muted />
+      <video ref={videoRef} src="/videos/video-03.mp4" autoPlay loop muted />
       <h1>
         Darik <br /> Coffee <br />
       </h1>
